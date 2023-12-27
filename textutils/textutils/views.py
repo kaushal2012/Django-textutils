c72bdc358401800854c80c6a11e25191ada7a467
kaushal2012 <kaushalsinhvadher@gmail.com>
2023-12-27 18:33:40 +0530

    commit

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -10,12 +10,25 @@ def index(request):
 def analyze(request):
     djtext = request.GET.get('text', 'default')
     removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
 
-    punctuations = string.punctuation
-    analyzed = ""
-    for char in djtext:
-        if char not in punctuations:
-            analyzed = analyzed + char
+    if removepunc == 'on':
+        punctuations = string.punctuation
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
 
-    params = {'purpose': 'Remove punctuation', 'analyzed_text': analyzed}
-    return render(request, 'analyze.html', params)
+        params = {'purpose': 'Remove punctuation', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif fullcaps == 'on':
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    else:
+        return HttpResponse('Error')
