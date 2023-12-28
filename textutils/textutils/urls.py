8f0c327d46b66508e0cde1d778733b093c393eb4
kaushal2012 <kaushalsinhvadher@gmail.com>
2023-12-28 18:46:16 +0530

    commit

--- textutils/textutils/urls.py
+++ textutils/textutils/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -24,3 +25,7 @@ urlpatterns = [
     path('analyze', views.analyze, name='analyze'),
 
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATICFILES_URL, document_root=settings.STATIC_ROOT)
