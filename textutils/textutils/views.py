62c604c1842c4cf50a1eb29c4e9d0fa6bcd8651b
kaushal2012 <kaushalsinhvadher@gmail.com>
2023-12-28 11:14:11 +0530

    commit

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
 
 
 def analyze(request):
     djtext = request.GET.get('text', 'default')
+
     removepunc = request.GET.get('removepunc', 'off')
     fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
 
     if removepunc == 'on':
         punctuations = string.punctuation
@@ -19,7 +21,7 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
 
-        params = {'purpose': 'Remove punctuation', 'analyzed_text': analyzed}
+        params = {'purpose': 'Remove punctuations', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', params)
 
     elif fullcaps == 'on':
@@ -27,7 +29,16 @@ def analyze(request):
         for char in djtext:
             analyzed = analyzed + char.upper()
 
-        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        params = {'purpose': 'Change to Uppercase', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif newlineremover == 'on':
+        analyzed = ""
+        for char in djtext:
+            if char != '\n':
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Remove New Lines', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', params)
 
     else:
